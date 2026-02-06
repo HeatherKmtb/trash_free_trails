dc8278a3678e47306fe6cb6acdfbd21a379bbbd6
HeatherKmtb <heather.kaymtb@gmail.com>
2026-02-06 16:10:58 +0000

    adding in some extra graphs to nature_connection

--- nature_connection.py
+++ nature_connection.py
@@ -18,7 +18,7 @@ df = df.dropna(subset=['TotItems'])
 sns.boxplot(x='NC', y='TotItems', data=df)
 plt.xlabel('Nature Connection')
 plt.ylabel('Number of items')
-plt.ylim(0, 1000) 
+#plt.ylim(0, 1000) 
 plt.title('Influence of total items on NC')
 plt.show()
 
@@ -37,3 +37,62 @@ f_oneway(*groups)
 #or non-parametric
 from scipy.stats import kruskal
 kruskal(*groups)
+
+
+
+
+#feel about amount
+ct = pd.crosstab(df['NC'], df['Connection_LitterFeel'])
+
+sns.heatmap(ct, annot=True, fmt='d', cmap='Greens')
+plt.xlabel('Connection_LitterFeel')
+plt.ylabel('Nature Connection')
+plt.title('How did you feel about the amount of litter you saw (4=disappointed)')
+plt.show()
+
+#more or less
+mapping = {'More!': 4, 'About the same.': 2, 'Less :)': 0, 'About the same':2}
+
+df['Connection_LitterAmount'] = (
+    df['Connection_LitterAmount']
+    .replace(mapping)
+    .pipe(pd.to_numeric, errors='coerce')
+    .astype('Int64'))
+
+#df = df[~df['Connection_LitterAmount'].isin([3, 1])]
+
+ct = pd.crosstab(df['NC'], df['Connection_LitterAmount'])
+
+sns.heatmap(ct, annot=True, fmt='d', cmap='Greens')
+plt.xlabel('Connection_LitterAmount')
+plt.ylabel('Nature Connection')
+plt.title('Did you find more or less SUP than expected (4=more)')
+plt.show()
+
+#after taking action
+ct = pd.crosstab(df['NC'], df['Connection_Action'])
+
+sns.heatmap(ct, annot=True, fmt='d', cmap='Greens')
+plt.xlabel('Connection_Action (4=proud)')
+plt.ylabel('Nature Connection')
+plt.title('How did you feel after taking action')
+plt.show()
+
+
+#taking action v more or less
+ct = pd.crosstab(df['Connection_Action'], df['Connection_LitterAmount'])
+
+sns.heatmap(ct, annot=True, fmt='d', cmap='Greens')
+plt.xlabel('Connection_LitterAmount (4=more)')
+plt.ylabel('Connection_Action (4=proud)')
+plt.title('Did you find more or less SUP than expected')
+plt.show()
+
+#taking action v feel about amount
+ct = pd.crosstab(df['Connection_Action'], df['Connection_LitterFeel'])
+
+sns.heatmap(ct, annot=True, fmt='d', cmap='Greens')
+plt.xlabel('Connection_LitterFeel (4=disappointed)')
+plt.ylabel('Connection_LitterAmount (4=more)')
+plt.title('How did you feel about the amount of litter you saw (4=disappointed)')
+plt.show()
